426ecc5b4be280454c533aeb83d65bbf72c0254b
Ubuntu <ubuntu@ip-172-31-10-33.ap-southeast-2.compute.internal>
2024-11-20 13:42:01 +0000

    added big fact main makefile

---
+++ big.c
@@ -0,0 +1,26 @@
+// C program to find the largest number among three number
+// using nested if-else
+#include <stdio.h>
+
+void  biggest()
+{
+    int a,b,c;
+	    printf("enter 3 numbers");
+    scanf("%d%d%d",&a,&b,&c);
+
+    // Finding largest by comparing using relational operators
+    if (a >= b) {
+        if (a >= c)
+            printf("%d is the largest number.", a);
+        else
+            printf("%d is the largest number.", c);
+    }
+    else {
+        if (b >= c)
+            printf("%d is the largest number.", b);
+        else
+            printf("%d is the largest number.", c);
+    }
+
+ //   return 0;
+}
